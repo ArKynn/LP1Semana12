817f343a9264255a7f5ac3b827b1c65acf176133
David Mendes <david.f.mendes@proton.me>
2023-06-02 13:04:39 +0100

    Create LP1Semana12 solution, HowManyOfThisType project and Checker class. Update Checker class and Program class.

---
+++ HowManyOfThisType/Checker.cs
@@ -0,0 +1,15 @@
+﻿namespace HowManyOfThisType;
+
+public static class Checker
+{
+    public static int HowManyOfType<T>(IEnumerable<object> items)
+    {
+        int numOfItems = 0;
+        foreach (var item in items)
+        {
+            if (item.GetType() == typeof(T)) numOfItems++;
+        }
+
+        return numOfItems;
+    }
+}
