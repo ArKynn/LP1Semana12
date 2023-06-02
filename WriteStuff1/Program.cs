f68b848943f35ed43b63ebb5a1035a93f1bd1776
David Mendes <david.f.mendes@proton.me>
2023-06-02 14:50:01 +0100

    Create WriteStuff1 Project and Update Program Class.

---
+++ WriteStuff1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace WriteStuff1;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        bool stop = false;
+        string strToAdd;
+        Queue<string> stringsToAdd = new Queue<string>();
+        
+        
+        while (!stop)
+        {
+            Console.WriteLine("Write here string to add to file!");
+            strToAdd = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(strToAdd)) stop = true;
+            else stringsToAdd.Enqueue(strToAdd);
+        }
+        File.WriteAllLines(args[0],stringsToAdd);
+    }
+}
