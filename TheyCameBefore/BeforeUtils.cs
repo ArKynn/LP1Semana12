e2a8fe1be106bd0e7f5ea97b82740ac134be1db2
David Mendes <david.f.mendes@proton.me>
2023-06-02 13:32:43 +0100

    Create TheyCameBefore Project, add BeforeUtils class and update BeforeUtils and program classes.

---
+++ TheyCameBefore/BeforeUtils.cs
@@ -0,0 +1,17 @@
+﻿namespace TheyCameBefore;
+
+public static class BeforeUtils
+{
+    public static IEnumerable<T> GetTheOnesBefore<T>(IEnumerable<T> enume, T num) where T : struct, IComparable
+    {
+        IEnumerable<T> finalEnum = Array.Empty<T>();
+
+        foreach (T item in enume)
+        {
+            if (item.CompareTo(num) < 0)
+            {
+                yield return item;
+            }
+        }
+    }
+}
